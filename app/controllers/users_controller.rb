fca9a75c5915e1e01f193ac00ca6c937b736abce
mizuho murata <m.m.cmyk56@gmail.com>
2017-03-03 11:23:43 +0000

    kadai1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
+  def show 
+   @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    if (current_user !=@user)
+       return redirect_to root_path
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if (@user.update(user_profile))
+      redirect_to @user
+    else
+      flash.now[:alert] = "Update Failed"
+      render 'edit'
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+  
+  def user_profile
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation, :area)
+  end
+end
